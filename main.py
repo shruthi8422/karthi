6c452ab5b68af6fc7af6045f07c9fe2bff4155db
shruthi8422 <111742854+shruthi8422@users.noreply.github.com>
2022-11-08 12:34:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+"""
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+app = FastAPI()
+templates = Jinja2Templates(directory ="templates")
+@app.get("/")
+def home():
+    return "Welcome"
+@app.get("/greet")
+def greet_user(name):
+
+    return "Welcome: " +name
+
+@app.get("/service")
+def user_service(uname,password):
+
+    return "Welcome " +uname,password
+    #return "Welcome " +uname+ " password  " +password
+
+   
+@app.get("/showGreetPage", response_class=HTMLResponse)
+def show_greet_page(request:Request):
+
+    return templates.TemplateResponse("greet.html", context={"request":request})
+"""
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory = "templates")
+# @-annotation 'get and post methods' of http protocol 
+#root url:http://127.0.0.1:8000/
+@app.get('/')
+def home():
+    '''
+    home page
+    '''
+    return "hello World"
+
+@app.get('/greet')
+def greet_user(name):
+    '''
+    checking username and password
+    '''
+    
+    name = input("Enter your Username: ")
+    password = int(input("Enter your Password: "))
+    if( password == 12345):
+        
+        
+        return "Successfully sign_in:" +name
